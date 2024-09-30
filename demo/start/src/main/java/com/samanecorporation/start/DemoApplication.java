bc184e00503224eb4dbf18400f9fceb866996378
Ngor Seck <ngorsecka@gmail.com>
2024-09-30 21:05:29 +0200

    upadte Started class

--- demo/start/src/main/java/com/samanecorporation/start/DemoApplication.java
+++ demo/start/src/main/java/com/samanecorporation/start/DemoApplication.java
@@ -5,11 +5,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication(scanBasePackages = "com.samanecorporation")
 @EnableJpaRepositories(basePackages = {"com.samanecorporation.adapter.repository.business"})
 @EntityScan(basePackages = "com.samanecorporation.adapter.repository.business.entity")
+@ComponentScan(basePackages = { "com.samanecorporation.adapter.api", "com.samanecorporation.domaine.service","com.samanecorporation.adapter.repository.business.client","com.samanecorporation.domaine.mapper","com.samanecorporation.adapter.repository.business.client"})
 public class DemoApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
